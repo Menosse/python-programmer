64341879f91cf22b12d21d139c248c5ce7504586
Menosse <famcsn2@gmail.com>
2020-05-18 16:36:33 -0300

    class practice

--- 1. Spyder python files/9.6_practical_questions_1_&_2_UNSOLVED.py
+++ 1. Spyder python files/9.6_practical_questions_1_&_2_UNSOLVED.py
@@ -13,7 +13,31 @@ a method of withdrawing money, depositing money and displaying the balance to
 the screen. Create an instance of the bank account and check that the methods
 work as expected.
 '''
+# class bankAccount(object):
+#     def __init__(self,balance=0.0):
+#         self.balance = balance
+    
+#     def displayBalance(self):
+#         print(f'Your account balance is {self.balance}')
+    
+#     def withdraw(self,amount=0.0):
+#         a = str(input(f'Would you like to withdraw {amount}? Y/N: ')).lower()
+#         if a == 'y':
+#             if amount <= self.balance:
+#                 self.balance = self.balance - amount
+#                 print(f'You have withdrawn {amount}, your new balance is {self.balance}!')
+#             else:
+#                 print('sorry, this amount is higher than your total account balance! Your Balance is {self.balance}')
+#         else:
+#             print('Operation canceled')
+#     def deposit(self):
+#         dep = float(input(f'how much would you like to deposit? '))
+#         self.balance =+ dep
+#         print(f'your new balance is {self.balance}')
+                
+# myaccount = bankAccount()
 
+    
 
 
 '''
@@ -21,3 +45,13 @@ Question 2
  Create a circle class that will take the value of a radius and
  return the area of the circle
 '''
+import math
+class circle(object):
+    def __init__(self,radius=1):
+        self.radius = radius
+    
+    def circleArea(self):
+        print(f'The area is {math.pi * (self.radius**2)}')
+        
+my_circle = circle(3)
+my_circle.circleArea()
